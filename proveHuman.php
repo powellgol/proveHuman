aeef97201951601d9ab3a0b102cb691097edce2f
Josh Powell <josh@goalkick.ca>
2014-10-18 17:52:25 -0700

    Updated proveHuman.php with descriptive comments for display & validation settings.

--- proveHuman.php
+++ proveHuman.php
@@ -15,16 +15,16 @@ $validationSettings = array(
 							);
 
 $displaySettings = array(
-							"imageWidth" => 240, // Width in Pixels
-							"imageHeight" => 50,
-							"borderWidth" => 2,
-							"borderHeight" => 2,
-							"borderColor" => array(0,0,0),
-							"backgroundColor" => array(255,255,255),
-							"fontFile" => "ttf/times.ttf",
-							"fontColor" => array(233,14,91),
-							"fontShadow" => array(0,0,0),
-							"fontSize" => "25",
+							"imageWidth" => 240, 				// Image Width in Pixels
+							"imageHeight" => 50,				// Image Height in Pixels
+							"borderWidth" => 2,					// Border Width in Pixels
+							"borderHeight" => 2,				// Border Height in Pixels
+							"borderColor" => array(0,0,0),		// Border Color in RGB
+							"backgroundColor" => array(255,255,255),	// Background Color in RGB
+							"fontFile" => "ttf/times.ttf",		// True type font
+							"fontColor" => array(233,14,91),	// Text Color in RGB
+							"fontShadow" => array(0,0,0),		// Text Shadow Color in RGB
+							"fontSize" => "25",					// Text Size
 							);
 
 $proveHuman = new proveHuman();
